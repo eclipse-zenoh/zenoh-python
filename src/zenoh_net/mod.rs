d389433f6b95e7102aff73bceadee395a1b3fa9b
Julien Enoch <julien.enoch@adlinktech.com>
2020-09-15 12:30:27 +0200

    Add zenoh.net.scout()

--- src/zenoh_net/mod.rs
+++ src/zenoh_net/mod.rs
@@ -12,7 +12,9 @@
 //   ADLINK zenoh team, <zenoh@adlink-labs.tech>
 //
 use crate::to_pyerr;
+use async_std::prelude::FutureExt;
 use async_std::task;
+use futures::prelude::*;
 use pyo3::prelude::*;
 use pyo3::wrap_pyfunction;
 
@@ -20,6 +22,7 @@ mod types;
 use types::*;
 mod session;
 use session::*;
+use zenoh::net::ZInt;
 
 // module zenoh.net
 #[pymodule]
@@ -92,6 +95,7 @@ sys.modules['zenoh.net.resource_name'] = resource_name
     m.add_class::<Reply>()?;
     m.add_class::<Session>()?;
     m.add_wrapped(wrap_pyfunction!(open))?;
+    m.add_wrapped(wrap_pyfunction!(scout))?;
     Ok(())
 }
 
@@ -99,4 +103,20 @@ sys.modules['zenoh.net.resource_name'] = resource_name
 fn open(config: Config) -> PyResult<Session> {
     let s = task::block_on(zenoh::net::open(config.c, None)).map_err(to_pyerr)?;
     Ok(Session::new(s))
+}
+
+#[pyfunction]
+fn scout(whatami: ZInt, iface: &str, scout_period: f64) -> Vec<Hello> {
+    task::block_on(async move {
+        let mut result = Vec::<Hello>::new();
+        let mut stream = zenoh::net::scout(whatami, iface).await;
+        let scout = async {
+            while let Some(h) = stream.next().await {
+                result.push(Hello { h })
+            }
+        };
+        let timeout = async_std::task::sleep(std::time::Duration::from_secs_f64(scout_period));
+        FutureExt::race(scout, timeout).await;
+        result
+    })
 }
