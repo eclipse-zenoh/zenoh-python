b34de02b279a61c36134fa4415216aa46b626a57
Julien Enoch <julien.enoch@adlinktech.com>
2020-10-05 15:11:56 +0200

    Add z_put_thr + z_sub_thr examples

---
+++ examples/zenoh/z_sub_thr.py
@@ -0,0 +1,90 @@
+# Copyright (c) 2017, 2020 ADLINK Technology Inc.
+#
+# This program and the accompanying materials are made available under the
+# terms of the Eclipse Public License 2.0 which is available at
+# http://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+# which is available at https://www.apache.org/licenses/LICENSE-2.0.
+#
+# SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+#
+# Contributors:
+#   ADLINK zenoh team, <zenoh@adlink-labs.tech>
+
+import sys
+import time
+import datetime
+import argparse
+import zenoh
+from zenoh import Zenoh
+from zenoh.net import Config
+
+# --- Command line argument parsing --- --- --- --- --- ---
+parser = argparse.ArgumentParser(
+    prog='z_sub',
+    description='zenoh sub example')
+parser.add_argument('--mode', '-m', dest='mode',
+                    default='peer',
+                    choices=['peer', 'client'],
+                    type=str,
+                    help='The zenoh session mode.')
+parser.add_argument('--peer', '-e', dest='peer',
+                    metavar='LOCATOR',
+                    action='append',
+                    type=str,
+                    help='Peer locators used to initiate the zenoh session.')
+parser.add_argument('--listener', '-l', dest='listener',
+                    metavar='LOCATOR',
+                    action='append',
+                    type=str,
+                    help='Locators to listen on.')
+
+args = parser.parse_args()
+config = Config(
+    mode=Config.parse_mode(args.mode),
+    peers=args.peer,
+    listeners=args.listener)
+
+# zenoh code  --- --- --- --- --- --- --- --- --- --- ---
+
+N = 100000
+
+count = 0
+start = None
+stop = None
+
+
+def print_stats(start, stop):
+    print("{:.6f} msgs/sec".format(N / (stop - start).total_seconds()))
+
+
+# Listener function triggered each time there is a change on a resource whose
+# path matches the selector
+def listener(changes):
+    global count, start, stop
+    if count == 0:
+        start = datetime.datetime.now()
+        count += 1
+    elif count < N:
+        count += 1
+    else:
+        stop = datetime.datetime.now()
+        print_stats(start, stop)
+        count = 0
+
+
+# initiate logging
+zenoh.init_logger()
+
+print("New zenoh...")
+zenoh = Zenoh(config)
+
+print("New workspace...")
+workspace = zenoh.workspace()
+
+print("Subscribe on {}".format('/test/thr'))
+sub = workspace.subscribe('/test/thr', listener)
+
+time.sleep(60)
+
+sub.close()
+zenoh.close()
