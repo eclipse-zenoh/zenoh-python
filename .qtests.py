aa3b76c495944635f59c3516b51b5a01f1236f16
Pierre Avital <pierre.avital@me.com>
2022-07-25 10:21:55 +0200

    remove some files that shouldn't have been there

--- .qtests.py
+++
@@ -1,9 +0,0 @@
-from zenoh import *
-
-init_logger()
-s: Session = Session({"mode": "peer"})
-print(s)
-
-print(KeyExpr("a/b"))
-print(KeyExpr.autocanonize("a/b"))
-print(KeyExpr.autocanonize("a/**/*"))
