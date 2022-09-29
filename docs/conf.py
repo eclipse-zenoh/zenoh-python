9f123fade29c825fa820f0b5cdb1cdc130e46c20
Julien Enoch <julien.enoch@adlinktech.com>
2022-09-29 17:31:34 +0200

    Set version to 0.6.0-beta.1

--- docs/conf.py
+++ docs/conf.py
@@ -35,7 +35,7 @@ copyright = '2020, ZettaScale Zenoh team, <zenoh@zettascale.tech>'
 author = 'ZettaScale Zenoh team, <zenoh@zettascale.tech>'
 
 # The full version, including alpha/beta/rc tags
-release = '0.6.0-dev'
+release = '0.6.0-beta.1'
 
 
 # -- General configuration ---------------------------------------------------
