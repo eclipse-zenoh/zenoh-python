9233db14efa7cbcdf151083ac67c65304e852035
Luca Cominardi <luca.cominardi@gmail.com>
2022-12-02 10:51:05 +0100

    Bump zenoh version to 0.7.0-rc

--- docs/conf.py
+++ docs/conf.py
@@ -35,7 +35,7 @@ copyright = '2020, ZettaScale Zenoh team, <zenoh@zettascale.tech>'
 author = 'ZettaScale Zenoh team, <zenoh@zettascale.tech>'
 
 # The full version, including alpha/beta/rc tags
-release = '0.6.0-beta.1'
+release = '0.7.0-rc'
 
 
 # -- General configuration ---------------------------------------------------
