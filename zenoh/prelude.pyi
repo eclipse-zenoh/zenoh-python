7df353bad00864c17191a4304543ab6bdd3160f0
Joseph Perez <joperez@hotmail.fr>
2024-04-22 13:06:44 +0200

    fix: don't use enum for encoding stub

--- zenoh/prelude.pyi
+++ zenoh/prelude.pyi
@@ -29,20 +29,20 @@ __all__ = [
     "Value",
 ]
 
-from enum import Enum, auto
 from typing import Self
 
-from zenoh.config import Config, WhatAmI, ZenohId
-from zenoh.key_expr import KeyExpr
-from zenoh.publication import CongestionControl, Priority
-from zenoh.query import ConsolidationMode, QueryTarget
-from zenoh.sample import Sample, SampleKind
-from zenoh.selector import Selector
-from zenoh.session import Session
-from zenoh.subscriber import Reliability
-from zenoh.value import Value
+from zenoh.config import Config as Config, WhatAmI as WhatAmI, ZenohId as ZenohId
+from zenoh.key_expr import KeyExpr as KeyExpr
+from zenoh.publication import CongestionControl as CongestionControl, Priority as Priority
+from zenoh.query import ConsolidationMode as ConsolidationMode, QueryTarget as QueryTarget
+from zenoh.sample import Sample as Sample, SampleKind as SampleKind
+from zenoh.selector import Selector as Selector
+from zenoh.session import Session as Session
+from zenoh.subscriber import Reliability as Reliability
+from zenoh.value import Value as Value
 
-class Encoding(Enum):
+
+class Encoding:
     """Default encoding values used by Zenoh.
     An encoding has a similar role to Content-type in HTTP: it indicates, when present, how data should be interpreted by the application.
     Please note the Zenoh protocol does not impose any encoding value nor it operates on it. It can be seen as some optional metadata that is carried over by Zenoh in such a way the application may perform different operations depending on the encoding value.
@@ -51,65 +51,237 @@ class Encoding(Enum):
 
     def __new__(cls, arg: IntoEncoding) -> Self: ...
 
-    ZENOH_BYTES = auto()
-    ZENOH_INT = auto()
-    ZENOH_UINT = auto()
-    ZENOH_FLOAT = auto()
-    ZENOH_BOOL = auto()
-    ZENOH_STRING = auto()
-    ZENOH_ERROR = auto()
-    APPLICATION_OCTET_STREAM = auto()
-    TEXT_PLAIN = auto()
-    APPLICATION_JSON = auto()
-    TEXT_JSON = auto()
-    APPLICATION_CDR = auto()
-    APPLICATION_CBOR = auto()
-    APPLICATION_YAML = auto()
-    TEXT_YAML = auto()
-    TEST_JSON5 = auto()
-    APPLICATION_PYTHON_SERIALIZED_OBJECT = auto()
-    APPLICATION_PROTOBUF = auto()
-    APPLICATION_JAVA_SERIALIZED_OBJECT = auto()
-    APPLICATION_OPENMETRICS_TEXT = auto()
-    IMAGE_PNG = auto()
-    IMAGE_JPEG = auto()
-    IMAGE_GIF = auto()
-    IMAGE_BMP = auto()
-    IMAGE_WEBP = auto()
-    APPLICATION_XML = auto()
-    APPLICATION_X_WWW_FORM_URLENCODED = auto()
-    TEXT_HTML = auto()
-    TEXT_XML = auto()
-    TEXT_CSS = auto()
-    TEXT_JAVASCRIPT = auto()
-    TEXT_MARKDOWN = auto()
-    TEXT_CSV = auto()
-    APPLICATION_SQL = auto()
-    APPLICATION_COAP_PAYLOAD = auto()
-    APPLICATION_JSON_PATCH_JSON = auto()
-    APPLICATION_JSON_SEQ = auto()
-    APPLICATION_JSONPATH = auto()
-    APPLICATION_JWT = auto()
-    APPLICATION_MP4 = auto()
-    APPLICATION_SOAP_XML = auto()
-    APPLICATION_YANG = auto()
-    AUDIO_AAC = auto()
-    AUDIO_FLAC = auto()
-    AUDIO_MP4 = auto()
-    AUDIO_OGG = auto()
-    AUDIO_VORBIS = auto()
-    VIDEO_H261 = auto()
-    VIDEO_H263 = auto()
-    VIDEO_H264 = auto()
-    VIDEO_H265 = auto()
-    VIDEO_H266 = auto()
-    VIDEO_MP4 = auto()
-    VIDEO_OGG = auto()
-    VIDEO_RAW = auto()
-    VIDEO_VP8 = auto()
-    VIDEO_VP9 = auto()
+    ZENOH_BYTES: Self
+    """Just some bytes.
+    
+    Constant alias for string: "zenoh/bytes"."""
+    ZENOH_INT: Self
+    """A VLE-encoded signed little-endian integer. Either 8bit, 16bit, 32bit, or 64bit. Binary reprensentation uses two's complement.
+    
+    Constant alias for string: "zenoh/int"."""
+    ZENOH_UINT: Self
+    """A VLE-encoded little-endian unsigned integer. Either 8bit, 16bit, 32bit, or 64bit.
+
+    Constant alias for string: "zenoh/uint"."""
+    ZENOH_FLOAT: Self
+    """A VLE-encoded float. Either little-endian 32bit or 64bit. Binary representation uses IEEE 754-2008 binary32 or binary64, respectively.
+
+    Constant alias for string: "zenoh/float"."""
+    ZENOH_BOOL: Self
+    """A boolean. 0 is false, 1 is true. Other values are invalid.
+
+    Constant alias for string: "zenoh/bool"."""
+    ZENOH_STRING: Self
+    """A UTF-8 string.
+
+    Constant alias for string: "zenoh/string"."""
+    ZENOH_ERROR: Self
+    """A zenoh error.
+
+    Constant alias for string: "zenoh/error"."""
+    APPLICATION_OCTET_STREAM: Self
+    """An application-specific stream of bytes.
+
+    Constant alias for string: "application/octet-stream"."""
+    TEXT_PLAIN: Self
+    """A textual file.
+
+    Constant alias for string: "text/plain"."""
+    APPLICATION_JSON: Self
+    """JSON data intended to be consumed by an application.
+
+    Constant alias for string: "application/json"."""
+    TEXT_JSON: Self
+    """JSON data intended to be human readable.
+
+    Constant alias for string: "text/json"."""
+    APPLICATION_CDR: Self
+    """A Common Data Representation (CDR)-encoded data.
+
+    Constant alias for string: "application/cdr"""
+    APPLICATION_CBOR: Self
+    """A Concise Binary Object Representation (CBOR)-encoded data.
+
+    Constant alias for string: "application/cbor"."""
+    APPLICATION_YAML: Self
+    """YAML data intended to be consumed by an application.
+
+    Constant alias for string: "application/yaml"."""
+    TEXT_YAML: Self
+    """YAML data intended to be human readable.
+
+    Constant alias for string: "text/yaml"."""
+    TEST_JSON5: Self
+    """JSON5 encoded data that are human readable.
+
+    Constant alias for string: "text/json5"."""
+    APPLICATION_PYTHON_SERIALIZED_OBJECT: Self
+    """A Python object serialized using pickle .
+
+    Constant alias for string: "application/python-serialized-object"."""
+    APPLICATION_PROTOBUF: Self
+    """An application-specific protobuf-encoded data.
+
+    Constant alias for string: "application/protobuf"."""
+    APPLICATION_JAVA_SERIALIZED_OBJECT: Self
+    """A Java serialized object.
+
+    Constant alias for string: "application/java-serialized-object"."""
+    APPLICATION_OPENMETRICS_TEXT: Self
+    """An openmetrics  data, common used by Prometheus .
+
+    Constant alias for string: "application/openmetrics-text"."""
+    IMAGE_PNG: Self
+    """A Portable Network Graphics (PNG) image.
+
+    Constant alias for string: "image/png"."""
+    IMAGE_JPEG: Self
+    """A Joint Photographic Experts Group (JPEG) image.
+
+    Constant alias for string: "image/jpeg"."""
+    IMAGE_GIF: Self
+    """A Graphics Interchange Format (GIF) image.
+
+    Constant alias for string: "image/gif"."""
+    IMAGE_BMP: Self
+    """A BitMap (BMP) image.
+
+    Constant alias for string: "image/bmp"."""
+    IMAGE_WEBP: Self
+    """A Web Protable (WebP) image.
+
+    Constant alias for string: "image/webp"""
+    APPLICATION_XML: Self
+    """An XML file intended to be consumed by an application..
+
+    Constant alias for string: "application/xml"."""
+    APPLICATION_X_WWW_FORM_URLENCODED: Self
+    """An encoded a list of tuples, each consisting of a name and a value.
+
+    Constant alias for string: "application/x-www-form-urlencoded"."""
+    TEXT_HTML: Self
+    """An HTML file.
+
+    Constant alias for string: "text/html"."""
+    TEXT_XML: Self
+    """An XML file that is human readable.
+
+    Constant alias for string: "text/xml"."""
+    TEXT_CSS: Self
+    """A CSS file.
+
+    Constant alias for string: "text/css"."""
+    TEXT_JAVASCRIPT: Self
+    """A JavaScript file.
+
+    Constant alias for string: "text/javascript"."""
+    TEXT_MARKDOWN: Self
+    """A MarkDown file.
+
+    Constant alias for string: "text/markdown"."""
+    TEXT_CSV: Self
+    """A CSV file.
+
+    Constant alias for string: "text/csv"."""
+    APPLICATION_SQL: Self
+    """An application-specific SQL query.
+
+    Constant alias for string: "application/sql"."""
+    APPLICATION_COAP_PAYLOAD: Self
+    """Constrained Application Protocol (CoAP) data intended for CoAP-to-HTTP and HTTP-to-CoAP proxies.
+
+    Constant alias for string: "application/coap-payload"."""
+    APPLICATION_JSON_PATCH_JSON: Self
+    """Defines a JSON document structure for expressing a sequence of operations to apply to a JSON document.
+
+    Constant alias for string: "application/json-patch+json"."""
+    APPLICATION_JSON_SEQ: Self
+    """A JSON text sequence consists of any number of JSON texts, all encoded in UTF-8.
+
+    Constant alias for string: "application/json-seq"."""
+    APPLICATION_JSONPATH: Self
+    """A JSONPath defines a string syntax for selecting and extracting JSON values from within a given JSON value.
+
+    Constant alias for string: "application/jsonpath"."""
+    APPLICATION_JWT: Self
+    """A JSON Web Token (JWT).
+
+    Constant alias for string: "application/jwt"."""
+    APPLICATION_MP4: Self
+    """An application-specific MPEG-4 encoded data, either audio or video.
+
+    Constant alias for string: "application/mp4"."""
+    APPLICATION_SOAP_XML: Self
+    """A SOAP 1.2 message serialized as XML 1.0.
+
+    Constant alias for string: "application/soap+xml"."""
+    APPLICATION_YANG: Self
+    """A YANG-encoded data commonly used by the Network Configuration Protocol (NETCONF).
+
+    Constant alias for string: "application/yang"."""
+    AUDIO_AAC: Self
+    """A MPEG-4 Advanced Audio Coding (AAC) media.
+
+    Constant alias for string: "audio/aac"."""
+    AUDIO_FLAC: Self
+    """A Free Lossless Audio Codec (FLAC) media.
+
+    Constant alias for string: "audio/flac"."""
+    AUDIO_MP4: Self
+    """An audio codec defined in MPEG-1, MPEG-2, MPEG-4, or registered at the MP4 registration authority.
+
+    Constant alias for string: "audio/mp4"."""
+    AUDIO_OGG: Self
+    """An Ogg-encapsulated audio stream.
+
+    Constant alias for string: "audio/ogg"."""
+    AUDIO_VORBIS: Self
+    """A Vorbis-encoded audio stream.
+
+    Constant alias for string: "audio/vorbis"."""
+    VIDEO_H261: Self
+    """A h261-encoded video stream.
+
+    Constant alias for string: "video/h261"."""
+    VIDEO_H263: Self
+    """A h263-encoded video stream.
+
+    Constant alias for string: "video/h263"."""
+    VIDEO_H264: Self
+    """A h264-encoded video stream.
+
+    Constant alias for string: "video/h264"."""
+    VIDEO_H265: Self
+    """A h265-encoded video stream.
+
+    Constant alias for string: "video/h265"."""
+    VIDEO_H266: Self
+    """A h266-encoded video stream.
+
+    Constant alias for string: "video/h266"."""
+    VIDEO_MP4: Self
+    """A video codec defined in MPEG-1, MPEG-2, MPEG-4, or registered at the MP4 registration authority.
+
+    Constant alias for string: "video/mp4"."""
+    VIDEO_OGG: Self
+    """An Ogg-encapsulated video stream.
+
+    Constant alias for string: "video/ogg"."""
+    VIDEO_RAW: Self
+    """An uncompressed, studio-quality video stream.
+
+    Constant alias for string: "video/raw"."""
+    VIDEO_VP8: Self
+    """A VP8-encoded video stream.
+
+    Constant alias for string: "video/vp8"."""
+    VIDEO_VP9: Self
+    """A VP9-encoded video stream.
+
+    Constant alias for string: "video/vp9"."""
 
     def with_schema(self, schema: str) -> Self:
         """Set a schema to this encoding. Zenoh does not define what a schema is and its semantichs is left to the implementer. E.g. a common schema for text/plain encoding is utf-8."""
+
 
 IntoEncoding = Encoding | str
