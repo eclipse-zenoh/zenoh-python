de13408a31bdc3f5d7c264ee38e53e2faa0e9409
Joseph Perez <joperez@hotmail.fr>
2024-05-06 13:56:45 +0200

    test: add z_ping/z_pong examples (#205)

---
+++ examples/z_pong.py
@@ -0,0 +1,89 @@
+#
+# Copyright (c) 2024 ZettaScale Technology
+#
+# This program and the accompanying materials are made available under the
+# terms of the Eclipse Public License 2.0 which is available at
+# http://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+# which is available at https://www.apache.org/licenses/LICENSE-2.0.
+#
+# SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+#
+# Contributors:
+#   ZettaScale Zenoh Team, <zenoh@zettascale.tech>
+#
+
+import argparse
+import json
+import time
+
+import zenoh
+
+# --- Command line argument parsing --- --- --- --- --- ---
+parser = argparse.ArgumentParser(prog="z_get", description="zenoh get example")
+parser.add_argument(
+    "--mode",
+    "-m",
+    dest="mode",
+    choices=["peer", "client"],
+    type=str,
+    help="The zenoh session mode.",
+)
+parser.add_argument(
+    "--connect",
+    "-e",
+    dest="connect",
+    metavar="ENDPOINT",
+    action="append",
+    type=str,
+    help="Endpoints to connect to.",
+)
+parser.add_argument(
+    "--listen",
+    "-l",
+    dest="listen",
+    metavar="ENDPOINT",
+    action="append",
+    type=str,
+    help="Endpoints to listen on.",
+)
+parser.add_argument(
+    "--config",
+    "-c",
+    dest="config",
+    metavar="FILE",
+    type=str,
+    help="A configuration file.",
+)
+
+args = parser.parse_args()
+conf = (
+    zenoh.Config.from_file(args.config) if args.config is not None else zenoh.Config()
+)
+if args.mode is not None:
+    conf.insert_json5("mode", json.dumps(args.mode))
+if args.connect is not None:
+    conf.insert_json5("connect/endpoints", json.dumps(args.connect))
+if args.listen is not None:
+    conf.insert_json5("listen/endpoints", json.dumps(args.listen))
+
+
+# Zenoh code  --- --- --- --- --- --- --- --- --- --- ---
+def main():
+    # initiate logging
+    zenoh.init_logger()
+
+    print("Opening session...")
+    session = zenoh.open(conf)
+
+    pub = session.declare_publisher(
+        "test/pong",
+        congestion_control=zenoh.CongestionControl.BLOCK(),
+    )
+    sub = session.declare_subscriber("test/ping", lambda s: pub.put(s.payload))
+
+    print("Press CTRL-C to quit...")
+    while True:
+        time.sleep(1)
+
+
+main()
