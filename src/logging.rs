e02acc94071d7858aeb5b67891f7aceb6c4b3214
Joseph Perez <joperez@hotmail.fr>
2024-06-03 17:34:07 +0200

    refactor: use `zenoh_util` for logging

--- src/logging.rs
+++ src/logging.rs
@@ -1,20 +1,9 @@
-use std::{fmt, thread};
+use std::thread;
 
 use pyo3::{prelude::*, sync::GILOnceCell, types::PyDict};
-use tracing::{
-    field::Field,
-    span::{Attributes, Record},
-    Event, Id, Subscriber,
-};
-use tracing_subscriber::{
-    layer::{Context, SubscriberExt},
-    registry::LookupSpan,
-};
+use zenoh_util::LogRecord;
 
-use crate::{
-    macros::import,
-    utils::{IntoPyResult, MapInto},
-};
+use crate::{macros::import, utils::MapInto};
 
 const LOGGER_NAME: &str = "zenoh";
 static LOGGER: GILOnceCell<PyObject> = GILOnceCell::new();
@@ -28,100 +17,41 @@ pub(crate) fn init_logger(py: Python) -> PyResult<()> {
     Ok(())
 }
 
-struct LogRecord {
-    level: tracing::Level,
-    file: Option<&'static str>,
-    line: Option<u32>,
-    message: Option<String>,
-    kwargs: Vec<(&'static str, String)>,
-}
-
-impl LogRecord {
-    fn emit(self, py: Python) -> PyResult<()> {
-        let level = match self.level {
-            tracing::Level::TRACE => 5,
-            tracing::Level::DEBUG => 10,
-            tracing::Level::INFO => 20,
-            tracing::Level::WARN => 30,
-            tracing::Level::ERROR => 40,
-        };
-        let logger = LOGGER.get(py).unwrap().bind(py);
-        if !logger
-            .call_method1("isEnabledFor", (level,))
-            .and_then(|obj| bool::extract_bound(&obj))
-            .unwrap_or(false)
-        {
-            return Ok(());
-        }
-        let factory = import!(py, logging.getLogRecordFactory).call0()?;
-        let kwargs = PyDict::new_bound(py);
-        for (k, v) in self.kwargs {
-            kwargs.set_item(k, v)?;
-        }
-        let record = factory.call(
-            (
-                LOGGER_NAME,
-                level,
-                self.file,
-                self.line,
-                self.message,
-                py.None(),
-                py.None(),
-            ),
-            Some(&kwargs),
-        )?;
-        logger.call_method1("handle", (record,))?;
-        Ok(())
+fn handle_record(py: Python, record: LogRecord) -> PyResult<()> {
+    let level = match record.level {
+        tracing::Level::TRACE => 5,
+        tracing::Level::DEBUG => 10,
+        tracing::Level::INFO => 20,
+        tracing::Level::WARN => 30,
+        tracing::Level::ERROR => 40,
+    };
+    let logger = LOGGER.get(py).unwrap().bind(py);
+    if !logger
+        .call_method1("isEnabledFor", (level,))
+        .and_then(|obj| bool::extract_bound(&obj))
+        .unwrap_or(false)
+    {
+        return Ok(());
     }
-}
-
-#[derive(Clone)]
-struct SpanFields(Vec<(&'static str, String)>);
-
-struct Layer(flume::Sender<LogRecord>);
-
-impl<S: Subscriber + for<'a> LookupSpan<'a>> tracing_subscriber::Layer<S> for Layer {
-    fn on_new_span(&self, attrs: &Attributes<'_>, id: &Id, ctx: Context<'_, S>) {
-        let span = ctx.span(id).unwrap();
-        let mut extensions = span.extensions_mut();
-        let mut fields = vec![];
-        attrs.record(&mut |field: &Field, value: &dyn fmt::Debug| {
-            fields.push((field.name(), format!("{value:?}")))
-        });
-        extensions.insert(SpanFields(fields));
+    let factory = import!(py, logging.getLogRecordFactory).call0()?;
+    let kwargs = PyDict::new_bound(py);
+    for (k, v) in record.attributes {
+        kwargs.set_item(k, v)?;
     }
-    fn on_record(&self, id: &Id, values: &Record<'_>, ctx: Context<'_, S>) {
-        let span = ctx.span(id).unwrap();
-        let mut extensions = span.extensions_mut();
-        let fields = extensions.get_mut::<SpanFields>().unwrap();
-        values.record(&mut |field: &Field, value: &dyn fmt::Debug| {
-            fields.0.push((field.name(), format!("{value:?}")))
-        });
-    }
-    fn on_event(&self, event: &Event<'_>, ctx: Context<'_, S>) {
-        let mut record = LogRecord {
-            level: *event.metadata().level(),
-            file: event.metadata().file(),
-            line: event.metadata().line(),
-            message: None,
-            kwargs: vec![],
-        };
-        if let Some(scope) = ctx.event_scope(event) {
-            for span in scope.from_root() {
-                let extensions = span.extensions();
-                let fields = extensions.get::<SpanFields>().unwrap();
-                record.kwargs.extend(fields.0.iter().cloned());
-            }
-        }
-        event.record(&mut |field: &Field, value: &dyn fmt::Debug| {
-            if field.name() == "message" {
-                record.message = Some(format!("{value:?}"));
-            } else {
-                record.kwargs.push((field.name(), format!("{value:?}")))
-            }
-        });
-        self.0.send(record).unwrap();
-    }
+    let record = factory.call(
+        (
+            LOGGER_NAME,
+            level,
+            record.file,
+            record.line,
+            record.message,
+            py.None(),
+            py.None(),
+        ),
+        Some(&kwargs),
+    )?;
+    logger.call_method1("handle", (record,))?;
+    Ok(())
 }
 
 #[pyfunction]
@@ -136,11 +66,10 @@ pub(crate) fn init_logging(
         import!(py, logging.basicConfig).call((), kwargs)?;
     }
     let (tx, rx) = flume::unbounded();
-    let subscriber = tracing_subscriber::registry().with(Layer(tx));
-    tracing::subscriber::set_global_default(subscriber).into_pyres()?;
+    zenoh_util::init_log_with_callback(move |record| tx.send(record).unwrap());
     thread::spawn(move || {
         for record in rx {
-            Python::with_gil(|gil| record.emit(gil)).ok();
+            Python::with_gil(|gil| handle_record(gil, record)).ok();
         }
     });
     Ok(())
