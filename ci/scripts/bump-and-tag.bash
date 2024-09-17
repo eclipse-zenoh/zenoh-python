78c90c3bf47a5c0368e0cc622c8e78d55937c0d0
Diogo Matsubara <diogo.matsubara@pm.me>
2024-09-17 16:55:15 +0200

    Update ci/scripts/bump-and-tag.bash
    Co-authored-by: Mahmoud Mazouz <mazouz.mahmoud@outlook.com>

--- ci/scripts/bump-and-tag.bash
+++ ci/scripts/bump-and-tag.bash
@@ -32,7 +32,7 @@ export GIT_COMMITTER_EMAIL=$git_user_email
 
 # For development releases (e.g. 1.0.0-dev-21-g2ca8632), transform the version
 # into a PEP-440 compatible version, since maturin>1 requires strict version compliance.
-if [[ "${version}" =~ [0-9]+\.[0-9]+\.[0-9]+-dev-[0-9]+-[a-z0-9]+ ]]; then
+if [[ "${version}" =~ [0-9]+\.[0-9]+\.[0-9]+-dev-[0-9]+-g[a-f0-9]+ ]]; then
   version=$(echo $version | sed 's/dev-/dev+/')
 
 # Bump Cargo version
