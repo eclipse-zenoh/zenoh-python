192f973292764f951618014fd1d557225b18eca1
Denis Biryukov <denis.biryukov@zettascale.tech>
2024-12-11 12:23:53 +0100

    add --cfg arg to examples

--- examples/common/common.py
+++ examples/common/common.py
@@ -46,6 +46,15 @@ def add_config_arguments(parser: argparse.ArgumentParser):
         action="store_true",
         help="Disable multicast scouting.",
     )
+    parser.add_argument(
+        "--cfg",
+        dest="cfg",
+        metavar="CFG",
+        default=[],
+        action="append",
+        type=str,
+        help="Allows arbitrary configuration changes as column-separated KEY:VALUE pairs. Where KEY must be a valid config path and VALUE must be a valid JSON5 string that can be deserialized to the expected type for the KEY field. Example: --cfg='transport/unicast/max_links:2'.",
+    )
 
 
 def get_config_from_args(args) -> zenoh.Config:
@@ -62,5 +71,13 @@ def get_config_from_args(args) -> zenoh.Config:
         conf.insert_json5("listen/endpoints", json.dumps(args.listen))
     if args.no_multicast_scouting:
         conf.insert_json5("scouting/multicast/enabled", json.dumps(False))
+
+    for c in args.cfg:
+        try:
+            [key, value] = c.split(":")
+        except:
+            print(f"`--cfg` argument: expected KEY:VALUE pair, got {c}")
+            raise
+        conf.insert_json5(key, value)
 
     return conf
