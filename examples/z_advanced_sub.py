25fdbc43ebd371043b912f24e0eb157b2b1feb93
Michael Ilyin <milyin@gmail.com>
2025-11-08 12:46:16 +0100

    exit corrected for passing tests

--- examples/z_advanced_sub.py
+++ examples/z_advanced_sub.py
@@ -51,7 +51,7 @@ def main(conf: zenoh.Config, key: str):
                 time.sleep(1)
         except KeyboardInterrupt:
             print("Interrupted by user. Shutting down...")
-            return -SIGINT  # simulate exit code on SIGINT
+    exit(-SIGINT)  # simulate exit code on SIGINT
 
 
 # --- Command line argument parsing --- --- --- --- --- ---
