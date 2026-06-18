28c33abb8d0595d6656369ae32f4a92ca8f1df85
Oussama Teffahi <70609372+oteffahi@users.noreply.github.com>
2026-06-18 19:26:37 +0200

    Expose timestamp instrumentation API (#736)
    * Add TimestampStack API
    
    * Add timestamp stack callback to Session API
    
    * Update zenoh git ref
    
    * Fix formatting
    
    * Fix clippy warning
    
    * Apply review comments
    
    * Add missing @property to stubs
    
    * Apply upstream API changes
    
    * Fix file formatting

--- src/lib.rs
+++ src/lib.rs
@@ -32,6 +32,7 @@ mod session;
 #[cfg(feature = "shared-memory")]
 mod shm;
 mod time;
+mod timestamp_stack;
 mod utils;
 
 use pyo3::prelude::*;
@@ -77,6 +78,10 @@ pub(crate) mod zenoh {
             Transport, TransportEvent, TransportEventsListener,
         },
         time::{Timestamp, TimestampId, NTP64},
+        timestamp_stack::{
+            InterceptionPoint, TimestampContext, TimestampInstrumentation,
+            TimestampInstrumentationBuilder, TimestampStack, TimestampStackRecord,
+        },
         ZError,
     };
 
